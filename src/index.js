5af98ec99ad80dc2313e57d220bab829ec3490a3
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-03-05 21:46:59 +0100

    backend ad endpoint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-const express = require('express');
-const cors = require('cors');
+//Importar bibliotecas
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
 
-// create and config server
+//CREAR VARIABLES
+
 const server = express();
-server.use(cors());
-server.use(express.json());
 
 // init express aplication
 const serverPort = 4000;
+
+// create and config server
+server.use(cors());
+server.use(express.json());
+
+// CONFIGURACIÓN DE MYSQL
+
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: process.env.MYSQL_PASS, // Cada una pone sus contraseñas - En terminal MYSQL_PASS='' npm run dev
+    database: "neflix",
+  });
+
+  await connection.connect();
+
+  console.log(
+    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+  );
+
+  return connection;
+}
+
+// Arrancar el servidor
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get ( 'apis/netflix' , (req, res) => {
-  res.json({info:"nada", result: [] }  );
+// Endpoint para obtener peliculas
+server.get("/api/movies", async (req, res) => {
+  const conn = await getConnection(); //conectar a la base de datos
+
+  //consultamos para obtener las peliculas
+
+  const queryGetMovies = `
+  SELECT * FROM neflix.movies;
+  `;
+
+  const [results] = await conn.query(queryGetMovies);
+
+  conn.close();
+
+  res.json({ info: "listado peliculas", results: results });
 });
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,11 +1,12 @@
 // login
 
-async function getMoviesFromApi({ appMovies }) {
+function getMoviesFromApi() {
   //   console.log('Se están pidiendo las películas de la app');
   //   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
   //   return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
   //     .then(response => response.json())
   //     .then(() => {
+
   //       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
   //       return {
   //         success: true,
@@ -29,13 +30,13 @@ async function getMoviesFromApi({ appMovies }) {
   //     });
   // };
 
-  const response = await fetch(
-    "//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json"
-  );
-  const data = await response.json();
-  appMovies = data;
+  return fetch("http://localhost:4000/movies")
+    .then((response) => response.json())
+    .then((data) => {
+      return data; //retorna los datos que obtiene del servidor
+    });
 }
-getMoviesFromApi();
+
 const objToExport = {
   getMoviesFromApi: getMoviesFromApi,
 };
